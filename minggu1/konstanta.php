3f436d0e78db500881ff075c7d935901e16df4a2
abiyuadilfi <abiyuadilfi>
2021-03-14 22:37:23 +0700

    bagus2

---
+++ minggu1/konstanta.php
@@ -0,0 +1,6 @@
+<?
+define ("NAMA", "M. Abiyyu Adilfi ");
+define ("NILAI", 90);
+echo "Nama : " . NAMA;
+echo "<br>Nilai : " . NILAI;
+?>
---
+++ minggu1/variabel.php
@@ -0,0 +1,6 @@
+<?php
+$nim = "A22.2020.02819";
+$nama = 'M. Abiyyu Adilfi';
+echo "NIM : " . $nim . "<br>";
+echo "Nama : $nama";
+?>
